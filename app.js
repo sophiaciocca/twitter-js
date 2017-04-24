1fd22cfb9ad9547d61196db8883a26fdd4b944ed
Betty Lin <bettyhappylin@gmail.com>
2017-04-24 17:59:02 -0400

    add root route, news route, and middleware

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use('*', function(req, res, next){
+  //console.log(req);
+  console.log(req.method + ' ' + req.baseUrl);
+  next();
+});
+
 app.listen(3000, () => {
   console.log('Server listening!');
 });
 
 app.get('/', function(req, res, next){
   res.send('<h1>HELLO THERE</h1>');
+});
+
+app.get('/news', function(req, res, next){
+  res.send('This is the news page');
 });
