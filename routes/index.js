90e02a0ca20d430ba34fa6f21378510e39cf5b0c
Betty Lin <bettyhappylin@gmail.com>
2017-04-25 11:59:40 -0400

    added/pre-populate tweet form

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetBank');
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extend: false}));
+router.use(bodyParser.json());
 
 router.get('/', function(req, res) {
+    var name = 'YourName';
     let tweets = tweetBank.list();
-    res.render('index', {tweets: tweets } );
+    res.render('index', {tweets: tweets, showForm: true, name: name } );
+});
+
+router.post('/tweets', function(req, res){
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
 });
 
 //router to get user page
 router.get('/users/:name', function(req, res, next) {
     var name = req.params.name;
     var tweets = tweetBank.find({name: name});
-    res.render('index', {tweets: tweets});
+    res.render('index', {tweets: tweets, showForm: true, name: name });
     console.log(tweets);
 });
 
